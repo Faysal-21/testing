ea7352549110ca32badfeb7385cbe445ffbc318d
faysal-21 <faysalsaxane060@gmail.com>
2025-02-19 20:46:27 +0300

    Done:

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn-click")
+const  qoraal = document.getElementById("paragraph")
+
+
+btn.addEventListener("click", ()=>{
+    qoraal.innerHTML = "Hello World"
+})
